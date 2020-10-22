14aba40e93555296c30a27253d08a72bd39bf06c
Vill Ain <vill99green@gmail.com>
2020-10-22 11:10:54 -0700

    Add a script that parses a single inventory

---
+++ parse_inventory.py
@@ -0,0 +1,50 @@
+# parse inventory
+import os
+import re
+import sys
+
+import mule_list
+
+garbage = ["Short Sword*",
+           "A Worn Candle",
+           "Skin of Milk",
+           "Bread Cakes*",
+           "Empty",
+           "Name",
+           "Song: Chant of Battle*",
+           "A tattered note",
+           "Tome of Order and Discord",
+           "Club*",
+           "Backpack",
+           "Large Sewing Kit"
+           ]
+
+def main():
+    try:
+        toon = sys.argv[1]
+    except:
+        print("Give the name of the character to parse")
+        quit()
+
+    inventory = get_inventory(toon)
+    if inventory:
+        print(toon)
+        print(inventory)
+
+
+def get_inventory(mule_name):
+    file_name = ".." + os.sep + mule_name + "-Inventory.txt"
+    item_list = []
+    try:
+        with open(file_name) as inventory:
+            for line in inventory:
+                split_line = re.split(r'\t+', line.rstrip('\t'))
+                if split_line[1] not in garbage:
+                    item_list.append(split_line[1])
+    except:
+        pass
+    return item_list
+
+
+if __name__ == '__main__':
+    main()
